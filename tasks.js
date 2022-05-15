6d9ed63505e981515eec62d42396ebaccf2f6684
Maren Lilleberre <marenlilleberre@gmail.com>
2022-05-16 00:02:42 +0200

    change date format, start input-row

--- tasks.js
+++ tasks.js
@@ -1,45 +1,42 @@
 const dataBase = [
   {
     taskName: "Clean shower",
-    lastDate: "12.04.2022",
+    lastDate: "12/04/2022",
     interval: "monthly",
-    reminder: false,
+    reminder: "No",
     category: "Cleaning",
   },
   {
     taskName: "Change the sheets",
-    lastDate: "29.04.2022",
+    lastDate: "29/04/2022",
     interval: "biweekly",
-    reminder: true,
+    reminder: "Yes",
     category: "Cleaning",
   },
   {
     taskName: "Change AC filter",
-    lastDate: "01.11.2021",
+    lastDate: "01/11/2021",
     interval: "yearly",
-    reminder: true,
+    reminder: "Yes",
     category: "Misc",
   },
   {
     taskName: "Clean living room",
-    lastDate: "05.03.2022",
+    lastDate: "05/03/2022",
     interval: "monthly",
-    reminder: false,
+    reminder: "No",
     category: "Cleaning",
   },
   {
     taskName: "Mow lawn",
-    lastDate: "07.05.2022",
+    lastDate: "07/05/2022",
     interval: "none",
-    reminder: false,
+    reminder: "No",
     category: "Garden",
   },
 ];
 
-/* Make example table */
-//th= table head
-//td= table data
-//tr= table row
+/* Make example table from array data*/
 
 const tasksSection = document.getElementById("tasks-container");
 
@@ -58,32 +55,33 @@ function makeTable(tableBody) {
     td = tr.insertCell(tr.cells.length);
     td.innerHTML = dataBase[i].reminder;
     td = tr.insertCell(tr.cells.length);
-    td.innerHTML = `<button>Done!</button>`;
+    td.innerHTML = `<button>I did it!</button>`;
   }
+  //make inputrow
+  const inputRow = tableBody.insertRow();
+  console.log(inputRow);
+  const taskNameInput = (inputRow.insertCell().innerHTML = `YO!`);
+
   //set id to all rows
   let rows = document.querySelectorAll("tr");
   let counter = 0;
   for (let j = 0; j < rows.length; j++) {
     counter++;
     rows[j].setAttribute("id", counter);
-    /*     dataBase[counter].id = counter;
-     */
   }
 }
 makeTable(dataBase);
-//set new date in "last-done-cell" when clicking done
 
-const buttons = tasksSection.querySelectorAll("button");
+//set new date in "last-done-cell" when clicking done
+const tableBody = document.querySelector("#taskData");
+const buttons = tableBody.querySelectorAll("button");
 for (let i = 0; i < buttons.length; i++) {
   const button = buttons[i];
-  console.log(button.parentElement.parentElement.id);
   button.addEventListener("click", (e) => {
     const clickedTask = e.target.parentElement.parentElement;
     const clickedTaskID = clickedTask.id;
     const objNum = parseInt(clickedTaskID - 2);
-    console.log(dataBase[objNum]);
     const currentDate = new Date().toLocaleDateString("en-GB");
-    console.log(currentDate);
     dataBase[objNum].lastDate = currentDate;
     console.log(clickedTask);
     clickedTask.childNodes[1].innerHTML = dataBase[objNum].lastDate;
